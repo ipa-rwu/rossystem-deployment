758164c6af3045ab568578543659704309fe4cf7
PN:Ruichao Wu2 <ruichao.wu@ipa.fraunhofer.de>
2021-11-17 13:49:29 +0100

    temporary fix: add other repo and rosdep source

---
+++ agriculture_robot_sprint3/extra_layer/extra.sh
@@ -0,0 +1,6 @@
+apt-get install wget
+wget https://packages.clearpathrobotics.com/public.key -O - | sudo apt-key add -
+sh -c 'echo "deb https://packages.clearpathrobotics.com/stable/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/clearpath-latest.list'
+apt-get update
+wget https://raw.githubusercontent.com/clearpathrobotics/public-rosdistro/master/rosdep/50-clearpath.list -O /etc/ros/rosdep/sources.list.d/50-clearpath.list
+apt-get dist-upgrade -y
